5f091bb5426b8c3338afc87ce98f522541bf042a
Mikhail-Tal-khal <khaledkhlyan@gmail.com>
2025-12-15 20:13:04 +0100

    oops with javascript

---
+++ Javascript/practise.js
@@ -0,0 +1,13 @@
+let age = 10;        // can change later
+const pi = 3.14;     // we keep this the same
+
+console.log(age);
+console.log(pi);
+
+age = 11;            // OK
+// pi = 3.14159;     // ❌ Not allowed, const can't be changed
+// let vs const
+
+// let → box that we can change.
+
+// const → box that we don’t want to change.
